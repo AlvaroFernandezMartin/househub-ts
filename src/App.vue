a52a32dda986ff4b33e74da8f51584a68ab7cb37
Alvaro <alvaro@gmail.com>
2025-05-02 18:39:44 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <NavbarHauses></NavbarHauses>
-  <router-view />
+  <div class="d-none d-sm-block">
+    <NavbarHauses />
+  </div>
+  <div class="page-content">
+    <router-view />
+  </div>
 </template>
-
 <script lang="ts" setup>
 import NavbarHauses from '@/components/NavbarHauses.vue'
 
 </script>
 
-<style >
-* {
-  margin: 0;
+<style>
+*{
   padding: 0;
+  margin: 0;
 }
+.page-content {
+  background-color: #f4f4f4;
+  min-height: 100vh;
 
+}
 </style>
--- src/views/NewHomePage.vue
+++ src/views/NewHomePage.vue
@@ -1,57 +1,62 @@
 <template>
-  <div class="container-fluid main">
-    <div class="row  min-vh-100 ">
-      <!-- Columna izquierda -->
-      <aside class="col-2 bg-warning d-none d-md-block ">
-       sadfasdf
-      </aside>
+  <div class="container-fluid ">
+    <div class="row justify-content-center min-vh-100 ">
 
-      <!-- Columna principal -->
-      <main class="col-8">
-        <div class="action-div">
-        <h1>Houses</h1>
-        <button id="button_create_new">+ CREATE NEW</button>
-        <div class="input-div">
-          <img id="img_search" src="../assets/ic_search@3x.png" alt="Buscar" class="icon" />
+      <!-- Lateral izquierdo, visible solo en sm+ -->
+      <aside class="d-none d-sm-block col-sm-2 "></aside>
+
+      <!-- Contenido central -->
+      <main class="col-12 col-sm-8 ">
+        <div class="row h-25">
+          <div class="d-flex justify-content-between p-0 bg-warning ">
+
+            <h1 class="m-0">Houses</h1>
+            <button id="button_create_new">+ CREATE NEW</button>
+          </div>
+
+          <div class="col-12 col-sm-6 bg-info">
+            <img src="../assets/ic_search@3x.png" alt="Buscar" class="icon" />
           <input type="text" placeholder="Search for a house" />
-          <button  id="button_clear">
-            <img
-              id="img_clear"
-              src="../assets/ic_clear@3x.png"
-              alt="clear_input"
-              class="icon"
-            />
+          <button>
+            <img src="../assets/ic_clear@3x.png" alt="clear" class="icon" />
           </button>
+          </div>
+          <div class="col-12 col-sm-6  bg-danger">
+            <button class="">Price</button>
+            <button class="">Size</button>
+          </div>
+
         </div>
-        <div class="button-div">
-          <button
-            id="button_price"
-          >
-            Price
-          </button>
-          <button
-            id="button_size"
-          >
-            Size
-          </button>
+        <div class="row">
         </div>
-      </div>
       </main>
 
-      <!-- Columna derecha -->
-      <aside class="col-2 bg-warning d-none d-md-block ">
-       sadfasdf
-      </aside>
+      <!-- Lateral derecho, visible solo en sm+ -->
+      <aside class="d-none d-sm-block col-sm-2 "></aside>
     </div>
   </div>
-
 </template>
 
 <script lang="ts" setup>
 </script>
 
 <style scoped>
-.main {
-  background-color: #f4f4f4;
+h1 {
+  font-family: 'Montserrat', sans-serif;
+}
+
+.icon {
+  width: 24px;
+  height: 24px;
+}
+#button_create_new{
+  background-color: #eb5440;
+  color: #ffffff;
+  height: 30px;
+  width: 150px;
+  text-align: center;
+  border-radius: 7px;
+  border: none;
+  cursor: pointer;
 }
 </style>
