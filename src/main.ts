9150d0fa0b4d61c98aa2bbe4ed1f06f7ae6734fd
Alvaro <alvarofernandezmartintr@gmail.com>
2025-05-22 18:14:11 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import 'bootstrap-icons/font/bootstrap-icons.css' 
-
 import { useUserStore } from '@/stores/user_info'
 
-const pinia = createPinia()
-const app = createApp(App)
+async function bootstrap() {
+  const app = createApp(App)
+  app.use(router)
+  app.use(createPinia())
 
-app.use(pinia)
-app.use(router)
+  const userStore = useUserStore()
+  await userStore.initUser()
 
-const userStore = useUserStore()
-await userStore.initUser()
+  app.mount('#app')
+}
 
-app.mount('#app')
+bootstrap()
